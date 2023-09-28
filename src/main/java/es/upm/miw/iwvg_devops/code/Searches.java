2c8d02f5125c89af4a40af497e36c47b5452aeae
ricardo.vente <ricardo751991@gmail.com>
2023-09-28 18:17:54 +0200

    fixing bug on findDecimalFractionByUserName #16

--- src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
@@ -31,9 +31,9 @@ public class Searches {
     public Stream<Double> findDecimalFractionByUserName(String name){
         return new UsersDatabase().findAll()
                 .filter(user -> name.equals(user.getName()))
-                .flatMap(user -> user.getFractions().stream()
-                        .filter(Objects::nonNull)
-                )
+                .flatMap(user -> user.getFractions()
+                        .stream()
+                        .filter(Objects::nonNull))
                 .map(Fraction::decimal);
     }
 
