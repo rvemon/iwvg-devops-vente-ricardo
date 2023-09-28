d0e04103f2e67482f6a467bf66fc7b2dba4b97e9
ricardo.vente <ricardo751991@gmail.com>
2023-09-28 15:42:11 +0200

    adding the third search "c" #13

--- src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
@@ -29,5 +29,14 @@ public class Searches {
         return resultFraction;
     }
 
+    public Stream<Double> findDecimalFractionByUserName(String name){
+        return new UsersDatabase().findAll()
+                .filter(user -> name.equals(user.getName()))
+                .flatMap(user -> user.getFractions().stream()
+                        .filter(Objects::nonNull)
+                )
+                .map(Fraction::decimal);
+    }
+
 
 }
